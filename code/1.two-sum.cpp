b3c72a3402dd095c3683fe746321319213b23751
Xianyi Meng <xymeng16@gmail.com>
2025-01-29 22:18:48 +0800

    1, 49

--- code/1.two-sum.cpp
+++ code/1.two-sum.cpp
@@ -1,33 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 // start_marker
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-      vector<int> result(2, 0);
-         std::map<int, int> diffs;
-
-         for (int i = 0; i < nums.size(); i++) {
-            diffs.insert({target - nums[i], i});
-         }
-
-         for (const auto& [d ,i]: diffs) {
-            if (i == nums.size() - 1) {
-                return result; // impossible
-            }
-
-            if (auto search = diffs.find(d); search != diffs.end()) {
-                result[0] = i;
-                result[1] = search->second + 1;
-                break;
-            }
-         }
-
-         return result;    
+  vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> hashtable;
+    for (int i = 0; i < nums.size(); i++) {
+      auto it = hashtable.find(target - nums[i]);
+      if (it != hashtable.end()) {
+        return {it->second, i};
+      }
+      hashtable[nums[i]] = i; // avoid self-matching
     }
+
+    return {};
+  }
 };
 // end_marker
 int main() {
-    Solution solution;
-
+  Solution solution;
+  vector<int> nums = {2, 7, 11, 15};
+  int target = 9;
+  vector<int> result = solution.twoSum(nums, target);
+  cout << result[0] << " " << result[1] << endl;
+  return 0;
 }
---
+++ code/49.group-anagrams.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+// start_marker
+
+struct vector_hash {
+  template <class T1, class T2>
+  std::size_t operator () (std::pair<T1, T2> const &v) const
+  {
+    return std::hash<T1>()(v.size());
+  }
+};
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      // unordered_map<char, unordered_set<int>> ht;
+      // for (int i = 0; i < strs.size(); i++) {
+      //   for (int j = 0; j < strs[i].size(); j++) {
+      //     ht[strs[i][j]].insert(i);
+      //   }
+      // }
+      vector<vector<string>> result;
+
+      auto sorted_strs = strs;
+      for (auto &str: sorted_strs) {
+        std::sort(str.begin(), str.end());
+      }
+
+      unordered_map<string, unordered_set<int>> hashmap;
+      for (int i = 0; i < sorted_strs.size(); i++) {
+        hashmap[sorted_strs[i]].insert(i);
+      }
+      for (const auto& [k, v]: hashmap) {
+        vector<string> t;
+        for (const auto& i: v) {
+          t.push_back(strs[i]);
+        }
+        result.push_back(t);
+      }
+
+      return result;
+    }
+};
+// end_marker
+int main() {
+    Solution solution;
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
+    auto result = solution.groupAnagrams(strs);
+    for (auto &v: result) {
+      for (auto &s: v) {
+        cout << s << ' ';
+      }
+      cout << endl;
+    }
+
+  return 0;
+}
